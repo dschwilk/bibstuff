bd74048e883d664c43f1c25ae92db2a5e67ce1cf
Alan G Isaac <alan.isaac@gmail.com>
2020-03-04 21:04:43 -0500

    replace tabs with spaces

--- scripts/bib4txt.py
+++ scripts/bib4txt.py
@@ -70,19 +70,19 @@ import simpleparse
 
 #local imports
 try:
-	from bibstuff import bibfile, bibgrammar, bibstyles, ebnf_sp
+    from bibstuff import bibfile, bibgrammar, bibstyles, ebnf_sp
 except ImportError: #allow user to run without installing
-	scriptdir = os.path.dirname(os.path.realpath(__file__))
-	bibdir = os.path.dirname(scriptdir)
-	sys.path.append(bibdir)
-	from bibstuff import bibfile, bibgrammar, bibstyles, ebnf_sp
+    scriptdir = os.path.dirname(os.path.realpath(__file__))
+    bibdir = os.path.dirname(scriptdir)
+    sys.path.insert(0, bibdir)
+    from bibstuff import bibfile, bibgrammar, bibstyles, ebnf_sp
 ################################################################################
 
 
 ###################  GLOBALS  ##################################################
 # some globals are set when this file is run as a script
-#	style
-#	bibfile_processor
+#    style
+#    bibfile_processor
 
 
 
@@ -100,36 +100,36 @@ def make_text_output(src_as_string,
                      parsed_bibfile,
                      style,
                      citations_only=True):
-	"""Create intext citations and the bibliography"""
-	#first: create a citation manager to handle the bibfile(s)
-	bib4txt_logger.debug('create citation manager')
-	citation_manager = style.CitationManager([parsed_bibfile],
-											citekeys=None,
-											citation_template=style.CITATION_TEMPLATE)
-	#second: create CiteRefProcessor object to process cites during src parsing
-	#        (and associate it with the citation_manager)
-	bib4txt_logger.debug('create cite processor')
-	cite_processor = bibstyles.shared.CiteRefProcessor(citation_manager)
-	#third: parse the text (ignore `taglist`; it is a dummy container)
-	bib4txt_logger.info('fill cite processor with keys')
-	taglist = src_parser.parse(src_as_string, processor=cite_processor)
-	"""
-	:note: Now cite_processor.all_citekeys holds the cite keys.
-	It is also associated with citation_manager which holds the bibliography,
-	so we can make a sorted entry list.  To do so need:
-		- the keys for the citations referenced
-		- a sort-key on which to base the sorting
-	:note: Sorting is style dependent---e.g., might sort entries on citation_rank.
-	"""
-	#set the citation manager citekeys to all found keys (an ordered list)
-	#citation_manager.citekeys = cite_processor.all_citekeys
-	#make the citation definitions for a list of References
-	bib4txt_logger.info('make citations')
-	result = citation_manager.make_citations()
-	#lastly, prepend the entire document, if desired
-	if not citations_only:
-		result = cite_processor.__repr__() + result
-	return result+'\n' # ds added newline 2008-06-27
+    """Create intext citations and the bibliography"""
+    #first: create a citation manager to handle the bibfile(s)
+    bib4txt_logger.debug('create citation manager')
+    citation_manager = style.CitationManager([parsed_bibfile],
+                                            citekeys=None,
+                                            citation_template=style.CITATION_TEMPLATE)
+    #second: create CiteRefProcessor object to process cites during src parsing
+    #        (and associate it with the citation_manager)
+    bib4txt_logger.debug('create cite processor')
+    cite_processor = bibstyles.shared.CiteRefProcessor(citation_manager)
+    #third: parse the text (ignore `taglist`; it is a dummy container)
+    bib4txt_logger.info('fill cite processor with keys')
+    taglist = src_parser.parse(src_as_string, processor=cite_processor)
+    """
+    :note: Now cite_processor.all_citekeys holds the cite keys.
+    It is also associated with citation_manager which holds the bibliography,
+    so we can make a sorted entry list.  To do so need:
+        - the keys for the citations referenced
+        - a sort-key on which to base the sorting
+    :note: Sorting is style dependent---e.g., might sort entries on citation_rank.
+    """
+    #set the citation manager citekeys to all found keys (an ordered list)
+    #citation_manager.citekeys = cite_processor.all_citekeys
+    #make the citation definitions for a list of References
+    bib4txt_logger.info('make citations')
+    result = citation_manager.make_citations()
+    #lastly, prepend the entire document, if desired
+    if not citations_only:
+        result = cite_processor.__repr__() + result
+    return result+'\n' # ds added newline 2008-06-27
 
 ################################################################################
 
@@ -137,128 +137,128 @@ def make_text_output(src_as_string,
 
 
 def bibfiles2string(bibfile_names):
-	bibfiles_as_strings = list()
-	for bibfile_name in bibfile_names:
-		if (os.path.splitext(bibfile_name)[-1]).lower() != ".bib":
-			bib4txt_logger.warning("%s does not appear to be a .bib file."%bibfile_name )
-		try:
-			with open(bibfile_name,'r') as fh:
-				bibfiles_as_strings.append( fh.read() )
-		except IOError:
-			bib4txt_logger.warning("%s not found."%bibfile_name )
-	return '\n'.join( bibfiles_as_strings )
-		
-		
+    bibfiles_as_strings = list()
+    for bibfile_name in bibfile_names:
+        if (os.path.splitext(bibfile_name)[-1]).lower() != ".bib":
+            bib4txt_logger.warning("%s does not appear to be a .bib file."%bibfile_name )
+        try:
+            with open(bibfile_name,'r') as fh:
+                bibfiles_as_strings.append( fh.read() )
+        except IOError:
+            bib4txt_logger.warning("%s not found."%bibfile_name )
+    return '\n'.join( bibfiles_as_strings )
+        
+        
 
 def main():
-	"""Command-line tool.  See bib4txt.py -h for help.
-	"""
+    """Command-line tool.  See bib4txt.py -h for help.
+    """
 
-	#set default input and output
-	_infile = sys.stdin
-	_outfile = sys.stdout
-	
-	from optparse import OptionParser
-	
-	_usage = """
-	usage: %prog [options] BIB_DATABASE
-	standard usage: %prog -i reST_FILE  -n -o refs_FILE BIB_DATABASE
-	"""
+    #set default input and output
+    _infile = sys.stdin
+    _outfile = sys.stdout
+    
+    from optparse import OptionParser
+    
+    _usage = """
+    usage: %prog [options] BIB_DATABASE
+    standard usage: %prog -i reST_FILE  -n -o refs_FILE BIB_DATABASE
+    """
 
-	parser = OptionParser(usage=_usage, version ="%prog " + __version__)
+    parser = OptionParser(usage=_usage, version ="%prog " + __version__)
 
-	parser.add_option("-i", "--infile", action="store", type="string", dest="infile",
-					  help="Parse FILE for citation references.", metavar="FILE")
-	parser.add_option("-o", "--outfile", action="store", type="string", dest="outfile",
-					  help="Write formatted references to FILE", metavar="FILE")
-	parser.add_option("-n", "--nuke", action="store_true", dest="overwrite", default=False,
-					  help="silently overwrite outfile, default=%default")
-	parser.add_option("-s", "--stylefile", action="store", dest="stylefile", default="default.py",
-					  help="Specify user-chosen style file",metavar="FILE")
-	parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False,
-					  help="Print INFO messages to stdout, default=%default")
-	parser.add_option("-V", "--very-verbose", action="store_true", dest="very_verbose", default=False,
-					  help="Print DEBUG messages to stdout, default=%default")
-	parser.add_option("-a", "--all", action="store_true", dest="entire_doc",
-		              default=False, help="Output entire document, making citation reference substitutions, default=%default")
-	parser.add_option("-x", "--xp", action="store_true", dest="xp_parse",
-		              default=False, help="Use experimental document parser, default=%default")
-	parser.add_option("-L", "--logger-level", action="store", type="int", dest="logger_level",
-					  help="Set logging level to integer value.")
+    parser.add_option("-i", "--infile", action="store", type="string", dest="infile",
+                      help="Parse FILE for citation references.", metavar="FILE")
+    parser.add_option("-o", "--outfile", action="store", type="string", dest="outfile",
+                      help="Write formatted references to FILE", metavar="FILE")
+    parser.add_option("-n", "--nuke", action="store_true", dest="overwrite", default=False,
+                      help="silently overwrite outfile, default=%default")
+    parser.add_option("-s", "--stylefile", action="store", dest="stylefile", default="default.py",
+                      help="Specify user-chosen style file",metavar="FILE")
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False,
+                      help="Print INFO messages to stdout, default=%default")
+    parser.add_option("-V", "--very-verbose", action="store_true", dest="very_verbose", default=False,
+                      help="Print DEBUG messages to stdout, default=%default")
+    parser.add_option("-a", "--all", action="store_true", dest="entire_doc",
+                      default=False, help="Output entire document, making citation reference substitutions, default=%default")
+    parser.add_option("-x", "--xp", action="store_true", dest="xp_parse",
+                      default=False, help="Use experimental document parser, default=%default")
+    parser.add_option("-L", "--logger-level", action="store", type="int", dest="logger_level",
+                      help="Set logging level to integer value.")
 
-	(options, args) = parser.parse_args()
-	if options.logger_level:
-		bib4txt_logger.setLevel(options.logger_level)
-	elif options.very_verbose:
-		bib4txt_logger.setLevel(logging.DEBUG)
-	elif options.verbose:
-		bib4txt_logger.setLevel(logging.INFO)
-	bib4txt_logger.info(
-			"\n".join([
-			"Script running:",
-			" args=%s",
-			" infile=%s",
-			" outfile=%s",
-			" style file=%s"
-			])%(args, options.infile, options.outfile,options.stylefile)
-			)
-	exec("import bibstuff.bibstyles.%s as style"%os.path.splitext(options.stylefile)[0])
+    (options, args) = parser.parse_args()
+    if options.logger_level:
+        bib4txt_logger.setLevel(options.logger_level)
+    elif options.very_verbose:
+        bib4txt_logger.setLevel(logging.DEBUG)
+    elif options.verbose:
+        bib4txt_logger.setLevel(logging.INFO)
+    bib4txt_logger.info(
+            "\n".join([
+            "Script running:",
+            " args=%s",
+            " infile=%s",
+            " outfile=%s",
+            " style file=%s"
+            ])%(args, options.infile, options.outfile,options.stylefile)
+            )
+    exec("import bibstuff.bibstyles.%s as style"%os.path.splitext(options.stylefile)[0])
 
-	# open output file for writing (default: stdout)
-	if options.outfile:
-		if os.path.exists(options.outfile) and not options.overwrite:
-			_msg = """ABORTED because output file %s already exists:
-			Use -n option to nuke (overwrite) this file.
-			PLEASE CHECK FILE NAME CAREFULLY!
-			"""%(options.outfile)
-			print(_msg)
-			sys.exit(1)
-		_outfile = open(options.outfile,'w')
+    # open output file for writing (default: stdout)
+    if options.outfile:
+        if os.path.exists(options.outfile) and not options.overwrite:
+            _msg = """ABORTED because output file %s already exists:
+            Use -n option to nuke (overwrite) this file.
+            PLEASE CHECK FILE NAME CAREFULLY!
+            """%(options.outfile)
+            print(_msg)
+            sys.exit(1)
+        _outfile = open(options.outfile,'w')
 
-	# read database (.bib) files
-	bibfile_names = args
-	bibfile_as_string = bibfiles2string(bibfile_names)
-	if not bibfile_as_string:
-		bib4txt_logger.warning("No BibTeX databases found.")
-		sys.exit(1)
+    # read database (.bib) files
+    bibfile_names = args
+    bibfile_as_string = bibfiles2string(bibfile_names)
+    if not bibfile_as_string:
+        bib4txt_logger.warning("No BibTeX databases found.")
+        sys.exit(1)
 
 
-	# read input file (default: stdin)
-	if options.infile:
-		try:
-			_infile = open(options.infile,'r')
-		except:
-			print "Cannot open: "+options.infile
-			sys.exit(1)
+    # read input file (default: stdin)
+    if options.infile:
+        try:
+            _infile = open(options.infile,'r')
+        except:
+            print("Cannot open: "+options.infile)
+            sys.exit(1)
 
-	if options.entire_doc:
-		ebnf_dec = ebnf_sp.cites_rest
-	else:
-		ebnf_dec = ebnf_sp.cites_only_rest
-	if options.xp_parse:
-		ebnf_dec = ebnf_sp.cites_xp
-	# Create a simpleparse.parser Parser based on the chosen grammar
-	cite_parser = simpleparse.parser.Parser(ebnf_dec, root='src')
+    if options.entire_doc:
+        ebnf_dec = ebnf_sp.cites_rest
+    else:
+        ebnf_dec = ebnf_sp.cites_only_rest
+    if options.xp_parse:
+        ebnf_dec = ebnf_sp.cites_xp
+    # Create a simpleparse.parser Parser based on the chosen grammar
+    cite_parser = simpleparse.parser.Parser(ebnf_dec, root='src')
 
-	# create object to store parsed .bib file
-	bibfile_processor = bibfile.BibFile()
-	bib4txt_logger.debug('Ready to parse bib file.')
-	#store parsed .bib files in the bibfile_processor
-	bibgrammar.Parse(bibfile_as_string, bibfile_processor)
-	bib4txt_logger.info('bib file parsed.')
+    # create object to store parsed .bib file
+    bibfile_processor = bibfile.BibFile()
+    bib4txt_logger.debug('Ready to parse bib file.')
+    #store parsed .bib files in the bibfile_processor
+    bibgrammar.Parse(bibfile_as_string, bibfile_processor)
+    bib4txt_logger.info('bib file parsed.')
 
-	result = make_text_output(
-		_infile.read(),
-		cite_parser,
-		bibfile_processor,
-		style,
-		citations_only = not options.entire_doc)
+    result = make_text_output(
+        _infile.read(),
+        cite_parser,
+        bibfile_processor,
+        style,
+        citations_only = not options.entire_doc)
 
-	_outfile.write(result)        
-	_outfile.close()
-	_infile.close()
+    _outfile.write(result)        
+    _outfile.close()
+    _infile.close()
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
