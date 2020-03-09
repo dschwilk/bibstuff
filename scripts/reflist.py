9fa58981ebda098543ef024e7c004f5eef1a9d21
Alan G Isaac <alan.isaac@gmail.com>
2020-03-09 11:56:27 -0400

    swtich from optparse to argparse

--- scripts/reflist.py
+++ scripts/reflist.py
@@ -19,8 +19,8 @@ when submitting to a journal.)
 """
 __docformat__ = "restructuredtext en"
 __authors__  =    ["Dylan W. Schwilk", "Alan G. Isaac"]
-__version__ = "1.5.3"
-__needs__ = '2.4'
+__version__ = "1.5.4"
+__needs__ = '2.7+'
 
 
 ###################  IMPORTS  ##################################################
@@ -29,67 +29,67 @@ import sys
 import logging
 
 #configure logger
-logging.basicConfig(format='\n%(levelname)s:\n%(message)s\n')
+logging.basicConfig(format='\n%(levelname)s:\n%(message)s\n', stream=sys.stderr)
 reflist_logger = logging.getLogger('bibstuff_logger')
 ################################################################################
 
 def get_braces_contents(s):
-	result = list()
-	word = list()
-	lvl = 0
-	for c in s:
-		if c == '}': lvl -= 1
-		if lvl > 0:
-			word.append(c)
-		if c == '{': lvl += 1
-		if lvl==0 and word:
-			result.append(''.join(word))
-			word = list()
-	return result
+    result = list()
+    word = list()
+    lvl = 0
+    for c in s:
+        if c == '}': lvl -= 1
+        if lvl > 0:
+            word.append(c)
+        if c == '{': lvl += 1
+        if lvl==0 and word:
+            result.append(''.join(word))
+            word = list()
+    return result
 
 def main():
-	"""Command-line tool"""
+    """Command-line tool"""
         
-	from optparse import OptionParser
-	
-	usage = """usage: %prog FILE
-	
-	For example, to create a stripped down database
-	for a particular latex document:
-	python %prog FILE.bbl | python bibsearch.py DB.bib -l > NEW_DB.bib
-	"""
+    from argparse import ArgumentParser
+    _usage = """usage: %(prog)s FILE
+    
+    For example, to create a stripped down database
+    for a particular latex document:
+    python %(prog)s FILE.bbl | python bibsearch.py DB.bib -l > NEW_DB.bib
+    """
+    parser = ArgumentParser(usage=_usage)
+    parser.add_argument('--version', action='version', version=__version__)
 
-	parser = OptionParser(usage=usage, version ="%prog " + __version__)
-	parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False,
-					  help="Print INFO messages to stdout, default=%default")
-	parser.add_option("-V", "--very_verbose", action="store_true", dest="very_verbose", default=False,
-					  help="Print DEBUG messages to stdout, default=%default")
-	   
-	(options, args) = parser.parse_args()
-	if options.verbose:
-		reflist_logger.setLevel(logging.INFO)
-	if options.very_verbose:
-		reflist_logger.setLevel(logging.DEBUG)
-	reflist_logger.info("Script running.\nargs=%s"%(args))
+    parser.add_argument("-V", "--verbosity", action="store", dest="verbosity",
+                      type=int, default=0,
+                      help="Print {1:INFO,2:DEBUG} messages to stderr, default=%(default)s")
+    parser.add_argument("bblFile", action="store")
+       
+    args = parser.parse_args()
+    if 1 == args.verbosity:
+        reflist_logger.setLevel(logging.INFO)
+    if 2 == args.verbosity:
+        reflist_logger.setLevel(logging.DEBUG)
+    reflist_logger.info("Script running with bbl file=%s"%(args.bblFile))
 
-	try :
-		src = open(args[0]).read()
-		reflist_logger.debug('successfully read %s'%args[0])
-	except :
-		src = sys.stdin.read()
+    try :
+        src = open(args.bblFile).read()
+        reflist_logger.debug('successfully read %s'%args.bblFile)
+    except :
+        src = sys.stdin.read()
 
-	items = src.split('\n\n')
-	for i in items :
-		i = i.strip()
-		if (i.startswith('\\bibitem')) :
-			i = i[8:].strip()
-			if i[0] == '[':
-				i = i[i.find(']') + 1:]
-			print(get_braces_contents(i)[0])
-		elif (i.startswith('\\harvarditem')) :
-			print(get_braces_contents(i)[2])
+    items = src.split('\n\n')
+    for i in items :
+        i = i.strip()
+        if (i.startswith('\\bibitem')) :
+            i = i[8:].strip()
+            if i[0] == '[':
+                i = i[i.find(']') + 1:]
+            print(get_braces_contents(i)[0])
+        elif (i.startswith('\\harvarditem')) :
+            print(get_braces_contents(i)[2])
 
 
 if __name__ == '__main__':
-	main()
+    main()
 
