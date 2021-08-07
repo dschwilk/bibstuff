0090c406677a35cfdf790894480a2aee8e319419
Alan G. Isaac <alan.isaac@gmail.com>
2021-08-06 20:22:57 -0400

    add test

--- tests/test_bibstuff/test_bibname.py
+++ tests/test_bibstuff/test_bibname.py
@@ -2,15 +2,16 @@
 """
 Provides tests for the bibstuff.bibname module
 
-:author: Dylan Schwilk
+:author: Dylan Schwilk and Alan Isaac
 :contact: http://www.schwilk.org
 :license: MIT (see `license.txt`_)
-:date: 2014-04-30
+:date: 2021-08-06
 
 .. _`license.txt`: ../../license.txt
 
 """
 
+#standard library imports:
 import unittest
 
 from bibstuff import bibname
@@ -34,3 +35,12 @@ class TestBibname(unittest.TestCase):
 		"LaTeX accents and non English characters"
 		n=bibname.BibName(r"J\orgen M\"{a}rtin and Sven \AAs")
 		self.assertEqual(n.get_names_dicts()[1]["first"][0], "Sven")
+
+	def test_first_middle(self):
+		"first and middle name vs last"
+		n=bibname.BibName("Alan Glen Isaac and Dylan Schwilk")
+		self.assertEqual(n.get_names_dicts()[0]["first"][0], "Alan")
+		self.assertEqual(n.get_names_dicts()[0]["first"][1], "Glen")
+		self.assertEqual(n.get_names_dicts()[0]["last"][0], "Isaac")
+		self.assertEqual(n.get_names_dicts()[1]["first"][0], "Dylan")
+		self.assertEqual(n.get_names_dicts()[1]["last"][0], "Schwilk")
